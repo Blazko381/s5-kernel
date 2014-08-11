251d16d29894f37f472b0c6fae646db70af82396
Will Deacon <will.deacon@arm.com>
2014-08-11 14:24:47 +0100

    asm-generic: add memfd_create system call to unistd.h
    Commit 9183df25fe7b ("shm: add memfd_create() syscall") added a new
    system call (memfd_create) but didn't update the asm-generic unistd
    header.
    
    This patch adds the new system call to the asm-generic version of
    unistd.h so that it can be used by architectures such as arm64.
    
    Cc: Arnd Bergmann <arnd@arndb.de>
    Reviewed-by: David Herrmann <dh.herrmann@gmail.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Change-Id: Ifeb7a8a2d98207e6020cca5fd5016d171f6cd23e
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- include/uapi/asm-generic/unistd.h
+++ include/uapi/asm-generic/unistd.h
@@ -692,9 +692,11 @@ __SC_COMP(__NR_process_vm_writev, sys_process_vm_writev, \
 __SYSCALL(__NR_kcmp, sys_kcmp)
 #define __NR_getrandom 278
 __SYSCALL(__NR_getrandom, sys_getrandom)
+#define __NR_memfd_create 279
+__SYSCALL(__NR_memfd_create, sys_memfd_create)
 
 #undef __NR_syscalls
-#define __NR_syscalls 274
+#define __NR_syscalls 275
 
 /*
  * All syscalls below here should go away really,
